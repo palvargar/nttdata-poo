815c6f6093e8cb64d3c7c60d389608501e9e43e3
palvarga <palvarga@emeal.nttdata.com>
2023-01-16 09:46:33 +0100

    Added Polymorphism Excercise with boxing and unboxing examples

---
+++ CalculatorProject/Polymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿using CalculatorProject.PlanDePensiones;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.Polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            Student student = new Student();
+            student.Name = "Pepe";
+
+            //Boxing amd AutoBoxing
+            arrayList.Add(student);
+
+            Console.WriteLine(((Student)arrayList[0]).Name); 
+
+            Object obj = new Student(); //Boxing
+            ((Student)obj).Name = "Alberto";
+            Console.WriteLine(((Student)obj).Name);
+        }
+    }
+}
